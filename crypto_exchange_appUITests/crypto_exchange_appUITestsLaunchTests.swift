c8e162f9a830914459a223d5d13be5e52898d63d
Felipe Augusto <felipe.asilva.ext@panonline.com.br>
2024-07-25 17:49:00 -0300

    Initial Commit

---
+++ crypto_exchange_appUITests/crypto_exchange_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  crypto_exchange_appUITestsLaunchTests.swift
+//  crypto_exchange_appUITests
+//
+//  Created by Felipe Augusto Silva on 25/07/24.
+//
+
+import XCTest
+
+final class crypto_exchange_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
